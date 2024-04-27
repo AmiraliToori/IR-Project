a0c75a64b0929c2c24371905dc5c3c54475abf04
AmiraliToori <master.masterblocker@gmail.com>
2024-04-27 13:49:50 +0330

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+ raw_dataset:
+#     doc_index += 1
+#     doc_path = "/home/glados/Documents/AmirAli Toori/Lessons/Python/IR-Project/docs" + "/doc" + str(doc_index) + ".txt"
+#     with open(doc_path, "w") as document:
+#         document.write(doc)
